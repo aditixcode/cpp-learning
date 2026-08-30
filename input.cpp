1af33b3dd2c04fa79f50c0fedb4c23f0589f66fb
aditixcode <aditi27october@gmail.com>
2026-08-30 15:51:55 +0530

    Add input practice

---
+++ input.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main () {
+    std::cout<<"Enter the numbers to multiply \n";
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<"The prduct of numbers is "<<a*b<<'\n';
+    return 0;
+}
